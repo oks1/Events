f801b4a716d135d36d212e67c0c461d6c5494fb7
Liutsiia <lusi_600@mail.ru>
2022-10-15 22:08:17 -0400

    MainWindows

--- Events/EventsDbContext.Designer.cs
+++ Events/EventsDbContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\aidar\Documents\GitHub\Events\Events\EventsDbContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\lusi_\Documents\John Abbott\Events\Events\EventsDbContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Events/MainWindow.xaml.cs
+++ Events/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Events
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
